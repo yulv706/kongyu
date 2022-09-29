d6d9d54af0034e7c1b13088ae6462338d967bd6c
kongyv <107789296+yulv706@users.noreply.github.com>
2022-09-29 22:06:44 +0800

    Add files via upload
    4.1.4覆盖的点数
    问题描述】
    
           给定坐标轴上的 n 个区间段，每个段的端点为整数坐标。有些段可以是一个点，可以彼此相交、相互嵌套，甚至重合，对于任意区间，0≤li ≤ ri≤6×105。
    
           任务如下：对于每个 k ∈[1..n]，计算被 k 个区间段覆盖的整数坐标点的个数。点 x被端点为 li 和 ri  的区间段覆盖，当且仅当 li≤ x ≤ri。
    
    【输入形式】
    
           输入的第一行为一个整数 n（1≤ n ≤ 2⋅ 105），区间段的个数。
    
           接下来的 n 行，每行一个整数对 li 和 ri，表示第 i 个区间的端点。
    【输出形式】
    
           输出 n 个用空格分隔的整数cnt1、cnt2、...、cntn，这里 cnti 为被 i 个区间覆盖的点的个数。
    (总是提示运行时错误，但错误没找出）

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@
 using namespace std;
 int main()
 {
-    int n,t,m=0;int T;cin>>T;
-    for(;T>0;T--){
-    cin>>n;t=n-1;int A[n];m=0;
-    for(;t>=0;t--)cin>>A[t];
-    for(t=1;t<n-1;t++)
-        if((A[t]-A[t-1])*(A[t]-A[t+1])<=0)m++;
-    if(m==0)cout<<"Yes\n";
-    else cout<<"No\n";}
+    const int i=60000;
+    int A[i]={0};
+    int a,b,c,n;cin>>n;
+    for(a=n;a>0;a--){
+        cin>>b>>c;
+        for(;b<=c;b++)A[b]++;
+    }
+    int B[n+1];int j=0;
+    for(;j<n+1;j++)B[j]=0;
+    for(a=0;a<=i-1;a++)if(A[a]>0){j=A[a];B[j]++;}
+    for(a=1;a<=n;a++)cout<<B[a]<<" ";
 }
